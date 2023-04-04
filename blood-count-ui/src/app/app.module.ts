34d0a13a2515c155d138b7aac97698f5c9800b02
s21105 <s21105@pjwstk.edu.pl>
2023-04-04 14:46:32 +0200

    Base environment

--- blood-count-ui/src/app/app.module.ts
+++ blood-count-ui/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { HomeComponent } from './core/home/home/home.component';
+import { HeaderComponent } from './core/header/header.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeLayoutComponent,
+    LoginLayoutComponent,
+    HomeComponent,
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ blood-count-ui/src/app/layouts/login-layout/login-layout.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-layout',
+  templateUrl: './login-layout.component.html',
+  styleUrls: ['./login-layout.component.css']
+})
+export class LoginLayoutComponent {
+
+}
