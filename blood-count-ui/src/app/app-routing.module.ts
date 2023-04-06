542e9c5bdb218a32dba1c0388373d78455f02556
s21105 <s21105@pjwstk.edu.pl>
2023-04-06 13:20:28 +0200

    Header added

--- blood-count-ui/src/app/app-routing.module.ts
+++ blood-count-ui/src/app/app-routing.module.ts
@@ -2,8 +2,22 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
 import { LoginComponent } from './login/login.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { AuthGuard } from './auth/auth.guard';
+import { HomeComponent } from './core/home/home.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    component: HomeLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ]
+  },
   {
     path: '',
     component: LoginLayoutComponent,
--- blood-count-ui/src/app/core/header/header.component.ts
+++ blood-count-ui/src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  constructor(private authService: AuthService) { }
 
+  onLogout() {
+    this.authService.logout();
+  }
 }
