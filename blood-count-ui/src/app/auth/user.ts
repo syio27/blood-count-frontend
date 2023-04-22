6ff892bf14477bb3b007691ce969013fbce3a916
Baglan Sagidulla <kxelly@mail.ru>
2023-04-22 04:41:08 +0200

    v1 login-register impl

--- blood-count-ui/src/app/auth/user.ts
+++
@@ -1,5 +0,0 @@
-export interface User {
-    userName: string;
-    password: string;
-    email: string;
-  }
--- blood-count-ui/src/app/components/registration/registration.component.ts
+++ blood-count-ui/src/app/components/registration/registration.component.ts
@@ -29,7 +29,7 @@ export class RegistrationComponent implements OnInit {
 
   ngOnInit() {
     this.form = this.fb.group({
-      userName: ['', Validators.required],
+      name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*\d)(?=.*[!@#$%^&*()_+-=])[0-9a-zA-Z!@#$%^&*()_+-=]{8,}$/)]],
       confirmPassword: ['', Validators.required],
@@ -46,7 +46,7 @@ export class RegistrationComponent implements OnInit {
 
   onSubmit() {
     if (this.form.valid) {
-      this.authService.register(this.form.value);
+      this.authService.register(this.form.value).subscribe();
     }
     this.formSubmitAttempt = true;
   }
