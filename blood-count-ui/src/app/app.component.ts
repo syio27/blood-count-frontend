15d9964a54d5230f53388eda90b3fa4b0868d57e
syio27 <baglansagidulladev@gmail.com>
2023-05-04 23:46:40 +0200

    remove access and user details when navigates to login page

--- blood-count-ui/src/app/app.component.ts
+++ blood-count-ui/src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 
-interface AppState { 
+interface AppState {
   message: string
 }
 @Component({
@@ -9,7 +11,31 @@ interface AppState {
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'blood-count-ui';
-  
+
+  constructor(private router: Router) {
+  }
+
+  ngOnInit() {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        if (event.url === '/login') {
+          this.removeAccess();
+          this.removeUserDetails();
+        }
+      });
+  }
+
+  private removeAccess() {
+    localStorage.removeItem("jwt");
+    localStorage.removeItem("expirationDate");
+  }
+
+  private removeUserDetails() {
+    localStorage.removeItem("userDetails");
+  }
 }
