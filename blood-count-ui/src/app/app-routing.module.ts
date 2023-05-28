bc79d8f5d9577a9172d78ac8f6d58f69c3447442
syio27 <baglansagidulladev@gmail.com>
2023-05-29 00:09:52 +0200

    node modules and package.json deleted

--- blood-count-ui/src/app/app-routing.module.ts
+++ blood-count-ui/src/app/app-routing.module.ts
@@ -13,7 +13,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomeLayoutComponent,
-    canActivate: [AuthGuard],
+    //canActivate: [AuthGuard],
     children: [
       {
         path: '',
