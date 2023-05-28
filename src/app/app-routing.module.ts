9e3980ff88b063c5776b29094752cb25ff7ad705
syio27 <baglansagidulladev@gmail.com>
2023-05-29 00:40:55 +0200

    folder refactor

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
   {
     path: '',
     component: HomeLayoutComponent,
-    //canActivate: [AuthGuard],
+    canActivate: [AuthGuard],
     children: [
       {
         path: '',
