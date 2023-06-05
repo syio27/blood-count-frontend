5019643ceef2506f532f202920504331d356cad9
syio27 <baglansagidulladev@gmail.com>
2023-06-06 00:22:49 +0200

    RefTable interface, service

---
+++ src/app/interfaces/IReferenceTable.ts
@@ -0,0 +1,9 @@
+export interface IReferenceTable {
+    id: number
+    parameter: string
+    unit: string
+    minFemale: number
+    maxFemale: number
+    minMale: number
+    maxMale: number
+}
