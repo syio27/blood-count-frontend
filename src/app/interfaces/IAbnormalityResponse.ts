0e92e5fee236ef46b63dc25ec7461672d2f0374d
syio27 <baglansagidulladev@gmail.com>
2023-06-10 04:39:05 +0200

    unit parameter added to interfaces of abnormality

--- src/app/interfaces/IAbnormalityResponse.ts
+++ src/app/interfaces/IAbnormalityResponse.ts
@@ -3,6 +3,7 @@ import { LevelTypes } from "../enums/levelType.enum"
 export interface IAbnormalityResponse {
     id: number
     parameter: string
+    unit: string
     minValue: number
     maxValue: number
     type: LevelTypes
--- src/app/interfaces/ICreateAbnormalityRequest.ts
+++ src/app/interfaces/ICreateAbnormalityRequest.ts
@@ -2,6 +2,7 @@ import { LevelTypes } from "../enums/levelType.enum"
 
 export interface ICreateAbnormalityRequest {
     parameter: string
+    unit: string
     minValue: number
     maxValue: number
     type: LevelTypes;
