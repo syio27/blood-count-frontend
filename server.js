d27775503c11a6d5e05f6cdc47864d5c8fe09d7f
syio27 <baglansagidulladev@gmail.com>
2023-06-22 02:53:07 +0200

    make it perfect

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/blood-count'));
+app.use(express.static(__dirname + '/dist/blood-count-ui'));
 
 app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname +
-        '/dist/blood-count/index.html'));
+        '/dist/blood-count-ui/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
