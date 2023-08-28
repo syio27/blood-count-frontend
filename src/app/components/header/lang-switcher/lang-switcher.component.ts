de7f53aeae594ee1cdcc6d2a52ed7d89e86236bf
syio27 <baglansagidulladev@gmail.com>
2023-08-28 13:30:37 +0200

    make selected lang in switcher sustainable accross whole app

--- src/app/components/header/lang-switcher/lang-switcher.component.ts
+++ src/app/components/header/lang-switcher/lang-switcher.component.ts
@@ -3,23 +3,26 @@ import { TranslateService } from '@ngx-translate/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { SharedLanguageService } from 'src/app/services/shared-lang.service';
 
-
 @Component({
   selector: 'app-lang-switcher',
   templateUrl: './lang-switcher.component.html',
   styleUrls: ['./lang-switcher.component.css']
 })
 export class LangSwitcherComponent {
-  currentLang: string
-  isExamPage: boolean
+  currentLang: string;
+  isExamPage: boolean;
 
   constructor(
     private translate: TranslateService,
     private router: Router,
     private langService: SharedLanguageService
   ) {
-    this.currentLang = 'pl'
+
+    this.currentLang = localStorage.getItem('currentLang') || 'en';
     translate.setDefaultLang(this.currentLang);
+    translate.use(this.currentLang);
+    this.langService.setLanguage(this.currentLang);
+
     router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         this.isExamPage = event.url === '/exam';
@@ -30,6 +33,8 @@ export class LangSwitcherComponent {
   switchLang(lang: string) {
     this.currentLang = lang;
     this.translate.use(lang);
-    this.langService.setLanguage(lang)
+    this.langService.setLanguage(lang);
+
+    localStorage.setItem('currentLang', lang);
   }
 }
