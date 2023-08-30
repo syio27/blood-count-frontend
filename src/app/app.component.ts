880bf511a869ab5459c31a6f4937f2b9585ed6b8
syio27 <baglansagidulladev@gmail.com>
2023-08-30 12:02:41 +0200

    remove uneccessery code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,6 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs/operators';
 import { AuthService } from './auth/auth.service';
-import { NgToastService } from 'ng-angular-popup';
-
-
-interface AppState {
-  message: string
-}
-
 
 @Component({
   selector: 'app-root',
@@ -21,11 +14,9 @@ export class AppComponent implements OnInit {
   constructor(
     private router: Router,
     private authService: AuthService,
-    private toast: NgToastService
   ) {
   }
 
-  
   ngOnInit() {
     this.router.events
       .pipe(
@@ -37,15 +28,4 @@ export class AppComponent implements OnInit {
         }
       });
   }
-  showSuccess() {
-    this.toast.success({ detail: "SUCCESS", summary: 'Your Success Message', duration: 5000 });
-  }
-  showError() {
-    this.toast.error({ detail: "ERROR", summary: 'Your Error Message', sticky: true });
-  }
-
-  showInfo() {
-    this.toast.info({ detail: "INFO", summary: 'Your Info Message', sticky: true });
-  }
-
 }
