e78c3e3f203e5b5f5366fc2757b0153bf34e1401
syio27 <baglansagidulladev@gmail.com>
2023-12-23 03:27:48 +0100

    remove unnecessary fields from interface

--- src/app/interfaces/IGameCurrentSessionState.ts
+++ src/app/interfaces/IGameCurrentSessionState.ts
@@ -1,10 +1,8 @@
 import { Pages } from '../enums/pages'
-import {SavedUserAnswerResponse} from './SavedUserAnswerResponse'
+import { SavedUserAnswerResponse } from './SavedUserAnswerResponse'
 
 export interface IGameCurrentSessionState {
     gameId: number,
-    estimatedEndTime: string,
     status: string,
-    savedUserAnswers: SavedUserAnswerResponse[],
     currentPage: Pages
 }
